6e9f9e4beedebdab46e6018b7e48df77f192b110
hanh vo <hahnvo422@gmail.com>
2023-10-08 20:59:44 -0500

    Initial Commit

---
+++ SwiftDataTest/SwiftDataTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataTestApp.swift
+//  SwiftDataTest
+//
+//  Created by Hanh Vo on 10/8/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
